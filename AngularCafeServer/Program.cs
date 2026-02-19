c7213c330540c461161945251d707521f2b33d14
Duyguukaya <Duyguukaya@users.noreply.github.com>
2026-02-19 21:46:22 +0300

    menu controller category ile ilişkilendirildi.

--- AngularCafeServer/Program.cs
+++ AngularCafeServer/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(config =>
     {
-        config.WithOrigins("http://localhost:4200/")
+        config.WithOrigins("http://localhost:4200")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
